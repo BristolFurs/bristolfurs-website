b987b303474dac7ea026a73fc183e1ba439aa19a
beeps <hi+github@berly.kim>
2024-01-20 14:12:00 +0000

    :construction: Workin'

--- src/_data/emergencyBannerText.js
+++ src/_data/emergencyBannerText.js
@@ -2,12 +2,21 @@
 // needs to be communicated (e.g. disruption due to Covid 2)
 
 module.exports = function () {
+  // Display the banner all the time on development environment
+  if (process.env.ENVIRONMENT === "development") {
+    return {
+      title: "Roadworks ahead 🚧",
+      body: `This is a development version of the website. Information on it may not be correct. See the <a class="bf-link" href="//bristolfurs.co.uk">main BristolFurs website</a> for the proper stuff.`,
+    }
+  }
+
   // Write a message to show in the banner. Uncomment to make it visible.
   // Only supports a single line of plain text at the moment.
-  return {
-    title: "Roadworks ahead 🚧",
-    body: "This website is still being actively developed. Information may not be up to date and some pages may appear unusual or broken.",
-  }
+  //
+  // return {
+  //   title: "The July meet has been cancelled",
+  //   body: "This is due to staff illness.",
+  // }
 
   return null
 }
