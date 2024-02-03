a506f4f28878686aa773a086fc356fec52938406
beeps <hi+github@berly.kim>
2024-02-03 16:23:17 +0000

    :recycle: Refactor code that calculates next meet date

--- src/_data/meetInfo.js
+++ src/_data/meetInfo.js
@@ -9,11 +9,6 @@ const overrides = {
   "2023-05-06": "2023-05-13",
 }
 
-const addMonths = require("date-fns/addMonths")
-const isFuture = require("date-fns/isFuture")
-const isToday = require("date-fns/isToday")
-const formatISO = require("date-fns/formatISO")
-
 module.exports = function () {
   return {
     schedule: [
@@ -21,57 +16,55 @@ module.exports = function () {
       { startTime: "1pm", name: "Furmeet at The Boardroom" },
       { startTime: "3pm", name: "Fursuit walk (weather permitting)" },
     ],
-    nextMeetDate: (function () {
-      // Get today's date
-      const today = new Date()
-
-      // Starting with the current month, loop until we have a first Saturday of the
-      // month that is in the future relative to today.
-      let iteration = 0
-      let isFound = false
-
-      do {
-        // Work out the first Saturday of the given month
-        let firstSaturday = firstSaturdayOfMonth(addMonths(today, iteration))
-        let firstSaturdayISO = formatISO(firstSaturday, { representation: "date" })
-
-        // Check to see if it's overridden
-        if (firstSaturdayISO in overrides) {
-          const overrideDate = overrides[firstSaturdayISO]
-
-          // If there's an alternative date set, use that instead. If there isn't
-          // (i.e. meet cancelled), set it to a past date so that it'll fail the
-          // next checks. It's nasty, but it works.
-          firstSaturday = overrideDate ? new Date(overrideDate) : new Date("1970-01-01")
-          firstSaturdayISO = formatISO(firstSaturday, { representation: "date" })
-        }
-
-        // Is this date today or in the future? If so, return it.
-        if (isFuture(firstSaturday) || isToday(firstSaturday)) {
-          isFound = true
-          return firstSaturdayISO
-        }
-
-        // Increment iteration
-        iteration++
-      } while (!isFound)
-
-      // If it's not, when is the first Saturday of the *next* month?
-      const firstSaturdayNextMonth = firstSaturdayOfMonth(addMonths(today, 1))
-    })(),
+    nextMeetDate: getNextNMeetDates(1),
+    next6MeetDates: getNextNMeetDates(6),
   }
 }
 
 // Helper function to check the first seven days of a month and return the
 // Saturday. We can be sure it'll be in the first seven days by the nature
 // of the Gregorian calendar.
-const firstSaturdayOfMonth = function (dateObj) {
-  const setDate = require("date-fns/setDate")
-  const isSaturday = require("date-fns/isSaturday")
+const getFirstSaturdayOfMonth = function (dateObj) {
+  const { setDate, isSaturday } = require("date-fns")
 
   for (let i = 1; i <= 7; i++) {
-    if (isSaturday(setDate(dateObj, i))) {
-      return setDate(dateObj, i)
+    const date = setDate(dateObj, i)
+    if (isSaturday(date)) {
+      return date
     }
   }
+}
+
+const getNextNMeetDates = function (iterations = 12, fromDate) {
+  const { addMonths, formatISO, isFuture, isToday } = require("date-fns")
+  const results = []
+
+  fromDate = fromDate || new Date()
+
+  // Use <= as we actually want `iterations + 1` loops to take place, in case
+  // the first one ends up being a past date
+  for (let i = 0; i <= iterations; i++) {
+    // Get first day of this month
+    let date = getFirstSaturdayOfMonth(addMonths(fromDate, i))
+
+    // Convert this date into ISO format for comparisons
+    let dateISO = formatISO(date, { representation: "date" })
+
+    // Check to see if this date has been overridden by a replacement date
+    if (dateISO in overrides) {
+      const overrideDate = overrides[dateISO]
+
+      // If there's an alternative date set, use that instead. If there isn't
+      // (i.e. meet cancelled), set it to a past date so that it'll fail the
+      // next checks. It's nasty, but it works.
+      date = new Date(overrideDate || "2000-01-01")
+      dateISO = formatISO(date, { representation: "date" })
+    }
+
+    if (isToday(date) || isFuture(date)) {
+      results.push(dateISO)
+    }
+  }
+
+  return results.slice(0, iterations)
 }
