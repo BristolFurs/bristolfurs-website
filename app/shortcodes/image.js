c5e9e315f0838c6c820c4a89ce5e9d59783917e3
beeps <hi+github@berly.kim>
2024-07-09 20:59:28 +0100

    :sparkles: Add attribution capability to images

--- app/shortcodes/image.js
+++ app/shortcodes/image.js
@@ -1,7 +1,7 @@
 const eleventyImg = require("@11ty/eleventy-img")
 const paths = require("../../tasks/paths")
 
-module.exports = function (src, alt, options = {}) {
+module.exports = function (src, alt, credit = "", options = {}) {
   // Throw errors is either required option is missing
   if (!src) {
     console.log(src)
@@ -22,41 +22,51 @@ module.exports = function (src, alt, options = {}) {
   }
   options = { ...defaultOptions, ...options }
 
+  // Output string
+  let pictureHtml
+
   // If this is an external image, we ain't processing that
   // Just create some HTML and spit it out
   if (src.substring(0, 7) === "http://" || src.substring(0, 8) === "https://" || src.substring(0, 2) === "//") {
-    return `<img ${options.class ? `class="${options.class}" ` : ""}src="${src}" alt="${alt}" loading="${
+    pictureHtml = `<img ${options.class ? `class="${options.class}" ` : ""}src="${src}" alt="${alt}" loading="${
       options.loading
     }" decoding="${options.decoding}">`
   }
 
-  // Prefix src with the default directory so it doesn't need manually
-  // including in every use
-  src = `${paths.srcImages}/${src}`
+  // Otherwise, proceed with the processing!
+  else {
+    // Prefix src with the default directory so it doesn't need manually
+    // including in every use
+    src = `${paths.srcImages}/${src}`
 
-  const generatorOptions = {
-    formats: options.formats,
-    widths: options.widths,
-    urlPath: options.urlPath,
-    urlPath: "/images/",
-    outputDir: paths.outputImages,
-    sharpOptions: {
-      animated: true,
-    },
+    const generatorOptions = {
+      formats: options.formats,
+      widths: options.widths,
+      urlPath: options.urlPath,
+      urlPath: "/images/",
+      outputDir: paths.outputImages,
+      sharpOptions: {
+        animated: true,
+      },
+    }
+
+    // Generate the images
+    eleventyImg(src, generatorOptions)
+
+    // Get metadata of the generated image
+    const metadata = eleventyImg.statsSync(src, generatorOptions)
+
+    // Generate the HTML needed to render out these lovely images
+    pictureHtml = eleventyImg.generateHTML(metadata, {
+      alt,
+      decoding: options.decoding,
+      loading: options.loading,
+      sizes: options.sizes,
+    })
   }
 
-  // Generate the images
-  eleventyImg(src, generatorOptions)
-
-  // Get metadata of the generated image
-  const metadata = eleventyImg.statsSync(src, generatorOptions)
-
-  // Generate the HTML needed to render out these lovely images
-  return eleventyImg.generateHTML(metadata, {
-    alt,
-    class: options.class,
-    decoding: options.decoding,
-    loading: options.loading,
-    sizes: options.sizes,
-  })
+  return `<figure class="bf-image${options.class ? ` ${options.class}` : ""}">
+    ${pictureHtml}
+    ${credit ? `<figcaption class="bf-image__attribution">${credit}</figcaption>` : ""}
+  </figure>`
 }
