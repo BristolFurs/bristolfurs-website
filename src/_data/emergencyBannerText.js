07af15fc6564f0455477de9084967db7f61e26e5
beeps <hi+github@berly.kim>
2024-12-10 14:23:03 +0000

    Revert ":ambulance: December meet cancelled"
    This reverts commit 6e6c8e9519cb55e6937b65d04a44922916a14c24.
    
    Messaging no longer needed as the meet date has now passed.

--- src/_data/emergencyBannerText.js
+++ src/_data/emergencyBannerText.js
@@ -5,10 +5,10 @@ export default function () {
   // Write a message to show in the banner. Uncomment to make it visible.
   // Only supports a single line of plain text at the moment.
 
-  return {
-    title: "The December meet has been cancelled",
-    body: "We have cancelled the meet on 7th December due to the severe weather warning in effect in Bristol and the surrounding areas. Do not travel to Bristol or the meet.",
-  }
+  // return {
+  //   title: "The July meet has been cancelled",
+  //   body: "This is due to staff illness.",
+  // }
 
-  // return null
+  return null
 }
--- src/_data/meetInfo.js
+++ src/_data/meetInfo.js
@@ -8,7 +8,7 @@ import { setDate, isSaturday, isFuture, isToday, addMonths, formatISO } from "da
 // e.g. If a meet is cancelled outright:
 // "2023-05-06": null
 const overrides = {
-  "2024-12-07": null,
+  "2023-05-06": "2023-05-13",
 }
 
 export default function () {
