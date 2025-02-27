37008d0072b257036e383ddc990ef15da714fd1a
beeps <hi+github@berly.kim>
2025-02-27 08:41:58 +0000

    :triangular_flag_on_post: Add alert about Boardroom closure

--- src/_data/emergencyBannerText.js
+++ src/_data/emergencyBannerText.js
@@ -5,10 +5,10 @@ export default function () {
   // Write a message to show in the banner. Uncomment to make it visible.
   // Only supports a single line of plain text at the moment.
 
-  // return {
-  //   title: "The July meet has been cancelled",
-  //   body: "This is due to staff illness.",
-  // }
+  return {
+    title: "The Boardroom has closed down.",
+    body: "Our normal meet venue has closed down at short notice. We're currently trying to find a new venue. Meets may be disrupted or cancelled until we secure a replacement.",
+  }
 
   return null
 }
