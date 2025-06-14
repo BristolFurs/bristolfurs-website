af02e535a1b603b7924f54df9ebb0713d4115de3
beeps <hi+github@berly.kim>
2025-06-14 13:53:16 +0100

    :wrench: Remove emergency banner

--- src/_data/emergencyBannerText.js
+++ src/_data/emergencyBannerText.js
@@ -5,10 +5,10 @@ export default function () {
   // Write a message to show in the banner. Uncomment to make it visible.
   // Only supports a single line of plain text at the moment.
 
-  return {
-    title: "Our venue has changed.",
-    body: "We have changed venues to the Colosseum in Redcliffe. We are working on updating all pages to account for this change.",
-  }
+  // return {
+  //   title: "Our venue has changed.",
+  //   body: "We have changed venues to the Colosseum in Redcliffe. We are working on updating all pages to account for this change.",
+  // }
 
   return null
 }
